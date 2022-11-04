556a98c639b703243cc67f9d127092bfec10a32d
georgos-g <gg@dancingbear.de>
2022-11-04 14:58:26 +0100

    added export plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,10 @@
 module.exports = ({ env }) => ({
-  "vercel-deploy": {
+  //...
+  "import-export-entries": {
     enabled: true,
-    config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
-      apiToken: process.env.VERCEL_API_TOKEN,
-      appFilter: "your-app-name-on-vercel",
-      teamFilter: "your-team-id-on-vercel",
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-    },
   },
+
+  //...
 });
 
 // module.exports = ({ env }) => {
